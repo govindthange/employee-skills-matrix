dc6f4cab5440dfc02bcfdf44e1c5ce65900c4b9b
Ajinkya <ajinkyad@winsoftech.com>
2023-02-27 21:06:54 +0530

    Create containerised workspace for all components #2

---
+++ employee-skills-service/src/main/java/org/util/employeeskillsservice/EmployeeSkillsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.util.employeeskillsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSkillsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSkillsServiceApplication.class, args);
+	}
+
+}
---
+++ employee-skills-service/src/test/java/org/util/employeeskillsservice/EmployeeSkillsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.util.employeeskillsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSkillsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
