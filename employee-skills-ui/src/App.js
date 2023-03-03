30672c86822271cbfa165dcec1e69881a4c023b0
Aniket <9981554081.sa@gmail.com>
2023-03-03 08:12:48 +0000

    Create layout for home page #14

--- employee-skills-ui/src/App.js
+++ employee-skills-ui/src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import EmpDetails from './components/emp-details';
 import FilterComponent from './components/filter-component';
-import Ag from './Ag'
+import Navbar from './components/Navbar'
+import AgGrid from './agGrid'
 import FileUploadComponent from './components/file-upload.component';
 import { Container, Grid, Button } from '@mui/material';
 
@@ -12,21 +13,19 @@ function App() {
     name: "pravin"
   }
   return (
+    <>
+    <Navbar />
     <Container maxWidth="xl">
       <Grid container spacing={2}>
         <Grid item xs={12} md={6} my={2}>
-          {/* <Button variant="outlined">Upload CSV</Button> */}
           <FileUploadComponent></FileUploadComponent>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <FilterComponent></FilterComponent>
-        </Grid>
         <Grid item xs={12} md={12}>
-          <EmpDetails {...empDetails} />
+          <AgGrid />
         </Grid>
       </Grid>
-      <Ag />
     </Container>
+    </>
   );
 }
 
