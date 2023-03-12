751355bba8786207b9311c9f685c8aca1797d29b
Ajinkya <ajinkyad@winsoftech.com>
2023-03-12 19:25:54 +0530

    Create database schema #4

--- employee-skills-ui/src/components/empoyee-skills-table/agGrid.js
+++ employee-skills-ui/src/components/empoyee-skills-table/agGrid.js
@@ -51,7 +51,7 @@ function App({ employees, fetchEmployeeData }) {
   }
 
   const handleFileUpload = () => {
-    this.fileUploadRef.current.click();
+    fileUploadRef.current.click();
   };
 
   function onDataExport() {
